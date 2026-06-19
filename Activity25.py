b7482e30c920a7f1412b2510c9af55aeeb776578
Justin19-jpg <justinsolina19@gmail.com>
2026-06-19 21:21:45 +0800

    Update Activity25.py

--- Activity25.py
+++ Activity25.py
@@ -9,8 +9,8 @@ print(programming_language)
 emtpy_dictionary = {} #empty dictionary
 # { KEY  VALUE } : dictionary has no index, it uses 'KEYS'
 
-pl_dictionary = {'ken' : 'java', 'clark' : 'C#', 'soli' : 'C++',\
+pl_dictionary = {'chiko' : 'java', 'clark' : 'C#', 'soli' : 'C++',\
                   'panget' : 'C', 'pogi' : 'Perl', 'lim' : 'Python',\
                       'minecraft' : 'JavaScript', 'lol' : 'Rust', 'estilo' : 'Getgood'}
 
-print(pl_dictionary['ken'])
+print(pl_dictionary['chiko'])
